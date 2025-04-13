495a6f670fe22bf2387e3abd9232a0441629fbd2
Karan Khatke <113087478+kkhatke@users.noreply.github.com>
2025-04-13 18:09:45 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "credentials" {
+  description = "Path to the key file"
+  default     = "../my-creds.json"
+}
+variable "project_id" {
+  description = "GCP Project ID"
+  default     = "dtc-de-project-454809"
+}
+variable "region" {
+  description = "GCP Region"
+  default     = "us-central1"
+}
+variable "location" {
+  description = "Project location"
+  default     = "US"
+}
+variable "gcs_bucket_name" {
+  description = "Storage bucket name"
+  default     = "fuel_n_fleet_bucket"
+}
+variable "gcs_bucket_class" {
+  description = "Storage bucket class"
+  default     = "STANDARD"
+}
+variable "bq_dataset_name" {
+  description = "BigQuery dataset name"
+  default     = "fuel_n_fleet"
+}
